5b721b05008f1315941090e9c393175281b2c39c
Imets <abajozsef04@gmail.com>
2026-03-05 18:56:55 +0200

    Felhasznaloi input es duplac hivas

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello Git - B verzio!\n");
+   
+    int n = 10;
+    double *x, *y;
+
+    doSomething(n, x, y);
+
+    printf("%f%f", &x, &y);
+
     return 0;
 }
